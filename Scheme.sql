2d6640010ba12d0029ae74d3f092850905c617c4
Muhammad Rizaldi <56159068+Zaldimr@users.noreply.github.com>
2021-07-02 18:13:20 +0700

    Update Scheme.sql

--- Scheme.sql
+++ Scheme.sql
@@ -3,41 +3,45 @@ CREATE DATABASE AWHumanResource
 GO
 USE AWHumanResource;
 
--- kalau mau bikin Schema, klik kanan folder Database Diagram --> New Database Diagram
-
--- [Query untuk membuat tabel di-execute dengan connect ke database AWHumanResource]
-
+DROP TABLE DimEmployee
 CREATE TABLE DimEmployee(
     EmployeeKey int
         constraint DimEmployee_pk
             primary key identity(1,1),
+ FirstName nvarchar(50),
+ LastName nvarchar(50),
  MaritalStatus nchar(1), 
  Gender nchar(1),
  BusinessEntityID int,
  BirthDate date,
- HireDate date
+ HireDate date,
+ VacationHours int,
+ SickLeaveHours int
 )
 
 --Membuat tabel Salary
+DROP TABLE DimSalary
 CREATE TABLE DimSalary(
     SalaryKey int
         constraint Salary_pk
             primary key identity(1,1),
- BusinessEntityID int,
+ BusinessEntityID int not null,
     Rate money
 )
 
 --Membuat tabel Departement
+DROP TABLE DimDepartment
 CREATE TABLE DimDepartment(
     DepartmentKey int
         constraint Department_pk
             primary key identity(1,1),
-    DepartmentID int,
+    DepartmentID int not null,
  Name nvarchar(50),
     GroupName nvarchar(50)
 )
 
 --Membuat tabel Time
+DROP TABLE DimTime
 CREATE TABLE DimTime(
  TimeKey int
   constraint Time_pk
@@ -48,6 +52,7 @@ CREATE TABLE DimTime(
  date datetime2(7)
 )
 --Membuat tabel FactTable
+DROP TABLE FactSalesPurchase
 CREATE TABLE FactSalesPurchase(
     OverallWork money,
     EmployeeKey int
@@ -59,9 +64,10 @@ CREATE TABLE FactSalesPurchase(
     DepartmentKey int
         constraint FactTable_Department_DepartmentKey_fk
             references DimDepartment,
-    TimeKey int
-        constraint FactTable_Time_TimeKey_fk
-            references DimTime
+	TimeKey int
+		constraint FactTable_Time_TimeKey_fk
+			references DimTime
 )
 
---ALTER TABLE AWHumanResource.DimFactTable ADD CONSTRAINT PK_FACTID PRIMARY KEY (SalesQuota, SalesYTD) --harusnya fact table gapake PK gapapa
+
+
